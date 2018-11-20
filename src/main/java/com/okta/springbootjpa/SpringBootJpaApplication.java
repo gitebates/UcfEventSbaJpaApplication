7cde74a1c121e431f7592e5e3e6a211c0ed5a75a
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-11-20 07:27:53 -0800

    creating start branch

--- src/main/java/com/okta/springbootjpa/SpringBootJpaApplication.java
+++ src/main/java/com/okta/springbootjpa/SpringBootJpaApplication.java
@@ -15,33 +15,5 @@ public class SpringBootJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
 	}
-
-	@Bean
-	ApplicationRunner init(KayakRepository repository) {
-
-		String[][] data = {
-				{"sea", "Andrew", "300.12", "NDK"},
-				{"creek", "Andrew", "100.75", "Piranha"},
-				{"loaner", "Andrew", "75", "Necky"}
-		};
-
-		return args -> {
-			Stream.of(data).forEach(array -> {
-				try {
-					Kayak kayak = new Kayak(
-							array[0],
-							array[1],
-					      	NumberFormat.getInstance().parse(array[2]),
-							array[3]
-					);
-					repository.save(kayak);
-				}
-				catch (ParseException e) {
-					e.printStackTrace();
-				}
-			});
-			repository.findAll().forEach(System.out::println);
-		};
-	}
 	
 }
